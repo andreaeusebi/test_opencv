11704f5e9481d9df36dcb3acc3ff96c0a6db4766
Andrea Eusebi <andrea.eusebi@toyota-industries.eu>
2024-03-19 10:33:34 +0100

    Improved script for display an image

--- scripts/display_image.py
+++ scripts/display_image.py
@@ -1,9 +1,27 @@
+import sys
+from pathlib import Path
 import matplotlib.pyplot as plt
 import cv2
 
-img = cv2.imread("/home/andrea/Downloads/aaaaa.png",
-                 flags=cv2.IMREAD_ANYDEPTH)
+if len(sys.argv) != 2:
+    print(f"Error! Expected 1 argument, while {len(sys.argv)-1} given.") 
+    exit()
 
-plt.imshow(img, cmap="gray")
+img_fpath = sys.argv[1]
+
+assert(Path(img_fpath).exists()), "Given image file path doesn't exist!"
+
+# Read image from file
+img = cv2.imread(img_fpath, flags=cv2.IMREAD_ANYCOLOR)
+
+# if it is a color image
+if (img.ndim == 3):
+    # switch color channels since openCV load them as BGR
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    plt.imshow(img)
+
+else: # grayscale img
+    plt.imshow(img, cmap="gray")
 
 plt.show()
